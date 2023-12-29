a6aaaa4ab6164562c3b131d0b58b52b058d26d19
Kevin Day <kevin.ch.day@gmail.com>
2023-12-29 03:11:49 -0600

    Alpha

--- dynamic_analysis/dynamic_main.py
+++ dynamic_analysis/dynamic_main.py
@@ -1,77 +1,202 @@
 import os
 import subprocess
+import tempfile
 import logging
-from datetime import datetime
+import shutil
 
 # Constants
 LOG_FILE = 'logs/dynamic_analysis.log'
-ANALYSIS_OUTPUT_DIR = 'analysis_results'
 
 # Configure logging
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def create_output_directory():
-    """Create the output directory if it doesn't exist."""
-    os.makedirs(ANALYSIS_OUTPUT_DIR, exist_ok=True)
-
-def perform_dynamic_analysis(apk_path):
+def check_devices():
     """
-    Perform dynamic analysis of an APK file.
+    Check if any Android devices or emulators are connected.
+
+    Returns:
+        bool: True if devices are connected, False otherwise.
+    """
+    try:
+        # Use the 'adb devices' command to list connected devices
+        adb_command = "adb devices"
+        output = subprocess.check_output(adb_command, shell=True, universal_newlines=True)
+
+        # Check if any devices/emulators are listed
+        if "List of devices attached" in output:
+            lines = output.split('\n')
+            connected_devices = [line.strip() for line in lines[1:] if line.strip()]
+            return len(connected_devices) > 0
+        else:
+            return False
+
+    except Exception as e:
+        logging.error(f"Error checking connected devices: {str(e)}")
+        return False
+
+def start_emulator():
+    """
+    Start the Android Emulator.
+
+    Returns:
+        bool: True if emulator started successfully, False otherwise.
+    """
+    try:
+        emulator_dir = tempfile.mkdtemp()
+        emulator_command = f"emulator -avd <YOUR_AVD_NAME>"
+        subprocess.run(emulator_command, shell=True, cwd=emulator_dir)
+        return True
+
+    except Exception as e:
+        logging.error(f"Error starting emulator: {str(e)}")
+        return False
+
+def install_apk(apk_path):
+    """
+    Install an APK file on the emulator.
 
     Args:
         apk_path (str): The path to the APK file.
 
     Returns:
-        dict: A dictionary containing the analysis results.
+        bool: True if installation successful, False otherwise.
     """
-    analysis_result = {
-        "APK Name": os.path.basename(apk_path),
-        "Analysis Time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "Analysis Status": "",
-        "Additional Information": ""
-    }
-
     try:
-        create_output_directory()
-        output_file = os.path.join(ANALYSIS_OUTPUT_DIR, f"{os.path.splitext(os.path.basename(apk_path))[0]}_dynamic_analysis.txt")
-
-        # Simulate dynamic analysis (replace with actual dynamic analysis steps)
-        analysis_result["Analysis Status"] = "Success"
-        analysis_result["Additional Information"] = "This is a simulated dynamic analysis result."
-
-        with open(output_file, "w") as f:
-            f.write("Dynamic Analysis Results:\n")
-            for key, value in analysis_result.items():
-                f.write(f"{key}: {value}\n")
-
-        logging.info(f"Dynamic analysis results saved to {output_file}")
+        install_command = f"adb install {apk_path}"
+        subprocess.run(install_command, shell=True)
+        return True
 
     except Exception as e:
-        logging.error(f"Error during dynamic analysis: {str(e)}")
-        analysis_result["Analysis Status"] = "Error"
-        analysis_result["Additional Information"] = str(e)
+        logging.error(f"Error installing APK: {str(e)}")
+        return False
 
-    return analysis_result
+def launch_app(package_name):
+    """
+    Launch an app on the emulator.
 
-def main():
-    print("Dynamic Analysis")
+    Args:
+        package_name (str): The package name of the app.
 
-    while True:
-        apk_path = input("Enter the path to the APK file for dynamic analysis (0 to exit): ")
-        
-        if apk_path == '0':
-            print("Exiting Dynamic Analysis. Goodbye!")
-            break
+    Returns:
+        bool: True if app launched successfully, False otherwise.
+    """
+    try:
+        launch_command = f"adb shell monkey -p {package_name} 1"
+        subprocess.run(launch_command, shell=True)
+        return True
 
-        if not os.path.exists(apk_path):
-            print("Invalid file path. Please provide a valid APK file path.")
-            continue
+    except Exception as e:
+        logging.error(f"Error launching app: {str(e)}")
+        return False
 
-        analysis_result = perform_dynamic_analysis(apk_path)
+def stop_emulator():
+    """
+    Stop the Android Emulator.
 
-        print("\nAnalysis Result:")
-        for key, value in analysis_result.items():
-            print(f"{key}: {value}")
+    Returns:
+        bool: True if emulator stopped successfully, False otherwise.
+    """
+    try:
+        stop_command = "adb emu kill"
+        subprocess.run(stop_command, shell=True)
+        return True
+
+    except Exception as e:
+        logging.error(f"Error stopping emulator: {str(e)}")
+        return False
+
+def perform_dynamic_analysis(apk_path):
+    """
+    Perform dynamic analysis on an APK file.
+
+    Args:
+        apk_path (str): The path to the APK file.
+
+    Returns:
+        dict: A dictionary containing the dynamic analysis results.
+    """
+    try:
+        print("Checking connected devices...")
+        if not check_devices():
+            return {
+                "Analysis Status": "Failure",
+                "Additional Information": "No Android devices or emulators are connected."
+            }
+
+        print("Starting the emulator...")
+        if not start_emulator():
+            return {
+                "Analysis Status": "Failure",
+                "Additional Information": "Failed to start the emulator."
+            }
+
+        print(f"Installing the APK: {apk_path}")
+        if not install_apk(apk_path):
+            return {
+                "Analysis Status": "Failure",
+                "Additional Information": "Failed to install the APK on the emulator."
+            }
+
+        package_name = get_package_name(apk_path)
+
+        if not package_name:
+            return {
+                "Analysis Status": "Failure",
+                "Additional Information": "Failed to retrieve the package name of the app."
+            }
+
+        print(f"Launching the app with package name: {package_name}")
+        if not launch_app(package_name):
+            return {
+                "Analysis Status": "Failure",
+                "Additional Information": "Failed to launch the app on the emulator."
+            }
+
+        # Monitor the app's behavior and collect dynamic analysis data here
+        # ...
+
+        print("Stopping the emulator...")
+        if not stop_emulator():
+            return {
+                "Analysis Status": "Failure",
+                "Additional Information": "Failed to stop the emulator."
+            }
+
+        return {
+            "Analysis Status": "Success",
+            "Additional Information": "Dynamic analysis completed successfully."
+        }
+
+    except Exception as e:
+        logging.error(f"Dynamic analysis failed with error: {str(e)}")
+        return {
+            "Analysis Status": "Failure",
+            "Additional Information": f"Dynamic analysis failed with error: {str(e)}"
+        }
+
+def get_package_name(apk_path):
+    """
+    Get the package name of an APK file.
+
+    Args:
+        apk_path (str): The path to the APK file.
+
+    Returns:
+        str: The package name, or an empty string if not found.
+    """
+    try:
+        aapt_command = f"aapt dump badging {apk_path} | grep package | awk '{{print $2}}' | sed s/name=//g | sed s/\\'/\\\"/g"
+        output = subprocess.check_output(aapt_command, shell=True, universal_newlines=True)
+        return output.strip()
+
+    except Exception as e:
+        logging.error(f"Error getting package name: {str(e)}")
+        return ""
 
 if __name__ == "__main__":
-    main()
+    print("Dynamic Analysis Script")
+    apk_path = input("Enter the path to the APK file: ")
+    analysis_result = perform_dynamic_analysis(apk_path)
+    print("\nAnalysis Result:")
+    print(f"Status: {analysis_result['Analysis Status']}")
+    print(f"Additional Information: {analysis_result['Additional Information']}")
--- test_dynamic_analysis.py
+++ test_dynamic_analysis.py
@@ -5,37 +5,32 @@ from unittest.mock import patch
 from dynamic_analysis import dynamic_main
 
 class TestDynamicAnalysis(unittest.TestCase):
-    def test_dynamic_analysis_success(self):
+    @patch('subprocess.run')
+    def test_dynamic_analysis_success(self, mock_run):
         apk_path = "SharkBot.apk"
-        
-        # Define a mock return value for subprocess.run
-        mock_run_result = MockRunResult(returncode=0, stdout="This is a simulated dynamic analysis result.")
-        
-        # Mock subprocess.run to return the defined result
-        with patch('subprocess.run', return_value=mock_run_result):
-            analysis_result = dynamic_main.perform_dynamic_analysis(apk_path)
-            
-            self.assertTrue(analysis_result["Analysis Status"] == "Success")
-            self.assertTrue("This is a simulated dynamic analysis result." in analysis_result["Additional Information"])
 
-    def test_dynamic_analysis_failure(self):
+        # Define a mock return value for subprocess.run
+        mock_run.return_value.returncode = 0
+        mock_run.return_value.stdout = "This is a simulated dynamic analysis result."
+        
+        analysis_result = dynamic_main.perform_dynamic_analysis(apk_path)
+        
+        self.assertTrue(analysis_result["Analysis Status"] == "Success")
+        self.assertTrue("This is a simulated dynamic analysis result." in analysis_result["Additional Information"])
+
+    @patch('subprocess.run')
+    def test_dynamic_analysis_failure(self, mock_run):
         apk_path = "Invalid.apk"
-        
-        # Define a mock return value for subprocess.run
-        mock_run_result = MockRunResult(returncode=1, stdout="", stderr="Error: Dynamic analysis failed.")
-        
-        # Mock subprocess.run to return the defined result
-        with patch('subprocess.run', return_value=mock_run_result):
-            analysis_result = dynamic_main.perform_dynamic_analysis(apk_path)
-            
-            self.assertTrue(analysis_result["Analysis Status"] == "Failure")
-            self.assertTrue("Error: Dynamic analysis failed." in analysis_result["Additional Information"])
 
-class MockRunResult:
-    def __init__(self, returncode, stdout, stderr):
-        self.returncode = returncode
-        self.stdout = stdout
-        self.stderr = stderr
+        # Define a mock return value for subprocess.run
+        mock_run.return_value.returncode = 1
+        mock_run.return_value.stdout = ""
+        mock_run.return_value.stderr = "Error: Dynamic analysis failed."
+        
+        analysis_result = dynamic_main.perform_dynamic_analysis(apk_path)
+        
+        self.assertTrue(analysis_result["Analysis Status"] == "Failure")
+        self.assertTrue("Error: Dynamic analysis failed." in analysis_result["Additional Information"])
 
 if __name__ == "__main__":
     unittest.main()
