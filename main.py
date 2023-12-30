dbc1432ca2c67631ed9e8a763047b6cb8c1a057e
Kevin Day <kevin.ch.day@gmail.com>
2023-12-30 16:35:17 -0600

    Alpha

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import logging
 import sys
 
 # Custom libraries
-from static_analysis.static_main import execute_static_analysis
-from dynamic_analysis.dynamic_main import execute_dynamic_analysis
+from static_analysis.static_main import static_analysis_alpha
+from dynamic_analysis.dynamic_main import perform_dynamic_analysis
 from utils.utils_main import create_output_directory, save_results
 from database.database_main import connect_to_database, store_analysis_result, retrieve_data
+from apk_analyzer import static_analysis_beta, change_model
+from model import load_model
 
 # Configure logging
 logging.basicConfig(filename='droidsecanalytica.log', level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
@@ -28,6 +30,8 @@ def display_menu(app_name="DroidSecAnalytica"):
     print(" 2. Dynamic Analysis")
     print(" 3. Utility Functions")
     print(" 4. Database Operations")
+    print(" 5. Change Model")
+    print(" 6. Load Model")
     print(" 0. Exit")
     print("=" * 24)
 
@@ -60,14 +64,15 @@ def display_app_name(app_name="DroidSecAnalytica"):
 def static_analysis_menu():
     print("\n Static Analysis Menu")
     print("=" * 24)
-    print(" 1. Perform Static Analysis")
-    print(" 2. Back to Main Menu")
+    print(" 1. Static Analysis Test Alpha")
+    print(" 2. Static Analysis Test Beta")
+    print(" 0. Back to Main Menu")
 
 # Dynamic analysis menu
 def dynamic_analysis_menu():
     print("\n Dynamic Analysis Menu")
     print(" 1. Perform Dynamic Analysis")
-    print(" 2. Back to Main Menu")
+    print(" 0. Back to Main Menu")
 
 # Utility functions menu
 def utility_functions_menu():
@@ -75,7 +80,7 @@ def utility_functions_menu():
     print("=" * 24)
     print(" 1. Create Output Directory")
     print(" 2. Save Results")
-    print(" 3. Back to Main Menu")
+    print(" 0. Back to Main Menu")
 
 # Database operations menu
 def database_operations_menu():
@@ -84,7 +89,7 @@ def database_operations_menu():
     print(" 1. Connect to Database")
     print(" 2. Store Analysis Result")
     print(" 3. Retrieve Data")
-    print(" 4. Back to Main Menu")
+    print(" 0. Back to Main Menu")
 
 # Exit the application
 def exit_app():
@@ -119,19 +124,26 @@ def main():
         elif choice == '1':
             static_analysis_menu()
             static_choice = input("\nEnter your choice: ")
+            apk_path = input("Enter the path to the APK file for static analysis: ")
+            if not os.path.exists(apk_path):
+                print("Invalid file path. Please provide a valid APK file path.")
+                continue
+            
+            # static test alpha
             if static_choice == '1':
                 print("\nStatic Analysis")
-                apk_path = input("Enter the path to the APK file for static analysis: ")
-                if not os.path.exists(apk_path):
-                    print("Invalid file path. Please provide a valid APK file path.")
-                    continue
-                execute_static_analysis(apk_path)
-                input("\nPress Enter to continue...")
+                static_analysis_alpha(apk_path)
+            
+            # static test beta
             elif static_choice == '2':
-                continue
+                results = static_analysis_beta(apk_path)
+                display_results_alpha(results)
+            
             else:
                 print("Invalid choice. Please select a valid option.")
                 continue
+
+            input("\nPress Enter to continue...")
 
         # Dynamic analysis
         elif choice == '2':
@@ -217,8 +229,39 @@ def main():
                 print("Invalid choice. Please select a valid option.")
                 continue
 
+        # Choice model
+        elif choice == '5':
+            change_model()
+        
+        # Load model
+        elif choice == '6':
+            model_path = input("Enter the path to the model file: ")
+            model = load_model(model_path)
+            # Use the loaded model as needed
+        
+        # Invalid
         else:
             print("Invalid choice. Please select a valid option.")
+
+# Display results alpha
+def display_results_alpha(results):
+    print("\nAPK Analysis Results\n")
+    for key, value in results.items():
+        title = key.capitalize()
+        print(f"---------------- {title} ----------------")
+        
+        if isinstance(value, list):
+            for index in value:
+                    print(" " + index)
+        
+        elif isinstance(value, dict):
+            for x in value:
+                print(" " + x)
+                for i in value[x]:
+                    print("\t" + i)
+        else:
+            print(f"{key}: {value}")
+        print()
 
 # CLI
 if __name__ == "__main__":
