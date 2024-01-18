91ad8ac41fc73fee6304e299cbbda546807ecbab
Kevin Day <kevinday612-softwaredev@outlook.com>
2024-01-17 19:04:23 -0600

    Update 1/17/2024

--- run.bat
+++ run.bat
@@ -1,1 +1,14 @@
-python main.py
+@echo off
+cls
+
+:: Check if Python is installed
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed or not in PATH.
+    pause
+    exit /b
+)
+
+python -W ignore main.py
+
+pause
