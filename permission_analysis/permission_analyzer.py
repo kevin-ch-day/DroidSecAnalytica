6e17586b938d43f25e40617417c2e187b58d4f8b
Kevin Day <kevinday612-softwaredev@outlook.com>
2024-02-10 01:47:42 -0600

    Update 2/10/2024

--- permission_analysis/permission_analyzer.py
+++ permission_analysis/permission_analyzer.py
@@ -6,60 +6,62 @@ from static_analysis import apk_decompilation
 from database import DBFunct_Perm
 from utils import user_prompts
 
-def handle_apk_permission_detection(apk_path):
+# Handle APK permission detection process
+def handle_apk_permission_detection(analysis_id, apk_path):
     decompiled_apk_path = apk_decompilation.decompile_apk(apk_path)
     permissions = extract_apk_permissions(decompiled_apk_path)
-    return process_permission(permissions)
+    return process_permission(analysis_id, permissions)
 
+# Extract permissions from the decompiled APK's manifest
 def extract_apk_permissions(decompiled_apk_path):
-    # Path to the AndroidManifest.xml file within the decompiled APK directory
     manifest_path = os.path.join(decompiled_apk_path, "AndroidManifest.xml")
     try:
+        # Parse the manifest file
         tree = ET.parse(manifest_path)
         root = tree.getroot()
     except ET.ParseError as e:
         print(f"Error parsing the manifest file: {e}")
         return []
     
-    # Namespace for Android attributes in the manifest file
+    # Define namespace for Android attributes
     ns = {'android': 'http://schemas.android.com/apk/res/android'}
-    
-    # Extract all uses-permission elements and their android:name attributes
+    # Extract permissions using the Android namespace
     permissions = [perm.attrib[f'{{{ns["android"]}}}name'] for perm in root.findall(".//uses-permission", ns)]
     
     return permissions
 
-def process_permission(permission):
-    try:
-        print(permission.name)
-        permission_record = DBFunct_Perm.get_permission_record_by_name(permission.name)
-        if permission_record:
-            process_standard_permission(permission_record, permission)
-        else:
-            process_unknown_permission(permission)
-    except Exception as e:
-        print(f"Error processing permission {permission.name}: {e}")
+# Process permissions extracted from the APK
+def process_permission(analysis_id, permissions):
+    for permission in permissions:
+        try:
+            # Find the permission record in the database
+            record = DBFunct_Perm.get_permission_record_by_name(permission)
+            if record:
+                # Process known permission
+                process_standard_permission(analysis_id, record, permission)
+            else:
+                # Process unknown permission
+                process_unknown_permission(analysis_id, permission)
+        except Exception as e:
+            print(f"Error processing permission {permission}: {e}")
 
-def process_standard_permission(permission_record, permission):
+# Process a known standard permission
+def process_standard_permission(analysis_id, permission_record, permission):
     id = permission_record[0]
-    DBFunct_Perm.check_standard_permission_record(id, permission.short_desc, permission.long_desc, permission.permission_type)
+    DBFunct_Perm.check_standard_permission_record(id, permission)
 
-def process_unknown_permission(permission):
+# Process an unknown permission
+def process_unknown_permission(analysis_id, permission):
     try:
-        unknown_permission_record = DBFunct_Perm.get_unknown_permission_record_by_name(permission.name)
+        unknown_permission_record = DBFunct_Perm.get_unknown_permission_record_by_name(permission)
         if unknown_permission_record:
-            id = unknown_permission_record[0]
-            print(f"Unknown Permission ID: {id}")
-            DBFunct_Perm.check_unknown_permission_record(id, permission.short_desc, permission.long_desc, permission.permission_type)
+            record_id = unknown_permission_record[0] 
+            print(f"Unknown Permission ID: {record_id}")
+            DBFunct_Perm.check_unknown_permission_record(record_id, permission)
         else:
             print("\n[**] Permission not found in database.")
-            print("Name:\t\t", permission.name)
-            print("Short Desc:\t", permission.short_desc)
-            print("Long Desc:\t", permission.long_desc)
-            print("Type:\t\t", permission.permission_type)
-            DBFunct_Perm.insert_unknown_permission_record(permission.name, permission.short_desc, permission.long_desc, permission.permission_type)
-            
+            print("Name:\t\t", permission)
+            DBFunct_Perm.insert_unknown_permission_record(permission)
             user_prompts.pause_until_keypress()
-
     except Exception as e:
         print(f"An error occurred while processing unknown permission: {e}")
