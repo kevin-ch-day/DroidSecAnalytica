e743b224eb501cfda69426105a03ea482c55106a
Kevin Day <kevinday612-softwaredev@outlook.com>
2024-03-19 13:02:10 -0500

    Update 3/19/2024

--- static_analysis/static_analysis.py
+++ static_analysis/static_analysis.py
@@ -1,15 +1,38 @@
+# static_analysis.py
+
 import os
 import subprocess
 import platform
 from typing import Optional
 
-from utils import app_utils, user_prompts, logging_utils, app_display
-from . import manifest_analysis, apk_decompilation
-from permission_audit import save_permissions
-from virustotal import vt_analysis, vt_requests
+from utils import logging_utils, app_utils
+from . import manifest_analysis
 
 # Constants for file paths
 ANALYSIS_OUTPUT_DIR = 'output'
+
+def handle_apk_decompilation():
+    apk_path = app_utils.android_apk_selection()
+    decompile_apk(apk_path, 'output')
+
+def decompile_apk(apk_path: str, output_directory: str) -> Optional[str]:
+    # Check OS and exit if Windows
+    if platform.system() == "Windows":
+        logging_utils.log_error("This function cannot be executed on Windows.")
+        return None
+
+    # Check if OS is Kali Linux or Ubuntu
+    if "kali" in platform.version().lower() or "ubuntu" in platform.version().lower():
+        try:
+            subprocess.run(["apktool", "d", "-f", apk_path, "-o", output_directory], check=True)
+            logging_utils.log_info(f"APK decompiled successfully. Output directory: {output_directory}")
+            return output_directory
+        except subprocess.CalledProcessError as e:
+            logging_utils.log_error(f"Error decompiling APK: {e}")
+            return None
+    else:
+        logging_utils.log_error("This function can only be executed on Kali Linux or Ubuntu.")
+        return None
 
 # Run static analysis
 def apk_static_analysis(apk_path: str):
@@ -17,7 +40,7 @@ def apk_static_analysis(apk_path: str):
     # local analysis
     file_basename = os.path.basename(apk_path)
     try:
-        output_directory = apk_decompilation.decompile_apk(apk_path, f"{ANALYSIS_OUTPUT_DIR}/{os.path.splitext(file_basename)[0]}")
+        output_directory = decompile_apk(apk_path, f"{ANALYSIS_OUTPUT_DIR}/{os.path.splitext(file_basename)[0]}")
         if output_directory:
             manifest_path = os.path.join(output_directory, "AndroidManifest.xml")
             manifest_data = manifest_analysis.analyze_android_manifest(manifest_path)
@@ -37,33 +60,3 @@ def apk_static_analysis(apk_path: str):
 
     except Exception as e:
         logging_utils.log_error(f"Error: virustotal analysis: {e}")
-
-def handle_sample_submission():
-    menu_title = "VirusTotal.com Sample Submission:"
-    menu_options = {
-        1: "APK",
-        2: "Hash",
-        0: "Return"
-    }
-    
-    app_display.display_menu(menu_title, menu_options)
-    choice = user_prompts.user_menu_choice("\nEnter your choice: ", ['0', '1', '2'])
-
-    if choice == '0':
-        return
-    
-    elif choice == '1':
-        apk_path = user_prompts.user_enter_apk_path()
-        vt_requests.query_apk(apk_path)
-        vt_data_analysis(response)
-        
-    elif choice == '2':
-        hash_ioc = user_prompts.user_enter_hash_ioc()
-        response = vt_requests.query_hash(hash_ioc)
-        vt_data_analysis(response)
-    
-    else:
-        print("Invalid choice. Please try again.")
-
-def vt_data_analysis(response):
-    vt_analysis.user_vt_data_processing(response)
